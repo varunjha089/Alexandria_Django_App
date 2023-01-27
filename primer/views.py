52e661b4ca34a666ea82951ac692caebf196c2be
Varun Kumar <varunjha089@gmail.com>
2023-01-27 22:52:26 +0530

    function based view with Hello World!

--- primer/views.py
+++ primer/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def say_hello(request):
+    message = "Hello world!"
+
+    return HttpResponse(message, content_type="text/plain")
